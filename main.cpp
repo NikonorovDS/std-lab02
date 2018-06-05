a64f8e7f24012f8db461c72a6f45c3cbc6bdc4cb
Никоноров Даниил <nikonorov@gmail.com>
2018-06-05 10:20:47 +0300

    dobavil stroku

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
-
+using namespace std;
 int
-main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+main(){
+    int a,b;
+    cout <<"vvedite A and B:";
+    cin >> a >>b;
+    cout <<"A+B="<<a+b<<endl;
+
 }
